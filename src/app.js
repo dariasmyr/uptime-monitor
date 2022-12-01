3757862b4ac8eceec4fa4205f2c199132e34d03e
daria_smyr <dasha.smyr@gmail.com>
2022-12-01 23:28:08 +0300

    Errir update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
-const sitesToFetch = require('./sample-config.json');
-const siteArrayLength = Object.keys(sitesToFetch.sites).length;
+const config = require('./sample-config.json');
 
-console.log(siteArrayLength);
+async function pingSite(url) {
+    // wait random time between 0 and 1 second
+    const randomDelay = Math.random() * 1000;
+    await new Promise(resolve => setTimeout(resolve, randomDelay));
+    console.log(`Pinged ${url}, time taken: ${randomDelay}ms`);
+}
+
+async function pingSites() {
+    const sites = config.sites;
+    for (const site of sites) {
+        console.log(`Pinging ${site.url}`);
+        pingSite(site.url);
+    }
+}
+
+pingSites();
+
+
 
