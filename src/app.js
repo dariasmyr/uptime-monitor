1f1a5ce39673d1973ed1771c033ce18b60aec019
daria_smyr <dasha.smyr@gmail.com>
2022-12-02 02:56:06 +0300

    added Promise.allSettled

--- src/app.js
+++ src/app.js
@@ -15,7 +15,28 @@ async function pingSites() {
     }
 }
 
-pingSites();
+// pingSites();
 
 
 
+function fetchUrl () {
+    let urls = [
+        'https://api.github.com/users/iliakan',
+        'https://api.github.com/users/remy',
+        'https://no-such-url'
+    ];
+
+    Promise.allSettled(urls.map(url => fetch(url)))
+        .then(results => { // (*)
+            results.forEach((result, num) => {
+                if (result.status === "fulfilled") {
+                    console.log(`${urls[num]}: ${result.value.status}`);
+                }
+                if (result.status === "rejected") {
+                    console.log(`${urls[num]}: ${result.reason}`);
+                }
+            });
+        });
+}
+
+fetchUrl();
