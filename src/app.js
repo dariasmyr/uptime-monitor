6594ecd6d6c86f90976fa761aa762f86d1d9778d
daria_smyr <dasha.smyr@gmail.com>
2022-12-03 13:33:14 +0300

    Small fixes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 const config = require('./sample-config.json');
 const axios = require('axios');
 
-function log(msg) {
-    console.log(new Date().toISOString(), msg);
+function log(...msg) {
+    console.log(new Date().toISOString(), ...msg);
 }
-
-// Mock function to ping a site
-// Todo - change this function as if you really want to ping a site
 
 async function pingSite(url) {
     try {
         // Download url and check status is 200
         const result = await axios.get(url);
-        return result.status === 200;
+        return (result.status >= 200) && (result.status <= 299);
     } catch (err) {
         log('err', err.message);
         return false;
@@ -20,19 +17,19 @@ async function pingSite(url) {
 }
 
 function main() {
+    console.log('Starting...');
     const sites = config.sites;
     log('sites', sites);
-    sites.forEach(function (site) {
+    for (const site of sites) {
         log(`Start pinging ${site.url} with interval ${site.intervalMs}`);
-
         setInterval(async function () {
-                const pingTimeStart = new Date().getTime();
-                const pingResult = await pingSite(site.url);
-                const pingTimeEnd = new Date().getTime();
-                const pingTime = pingTimeEnd - pingTimeStart;
-                log(`Ping ${site.url} result: ${pingResult} in ${pingTime} ms`);
+            const pingTimeStart = new Date().getTime();
+            const pingResult = await pingSite(site.url);
+            const pingTimeEnd = new Date().getTime();
+            const pingTime = pingTimeEnd - pingTimeStart;
+            log(`Ping ${site.url} result: ${pingResult} in ${pingTime} ms`);
         }, site.intervalMs);
-    });
+    }
     log('Monitoring sites...');
 }
 
