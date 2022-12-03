5eefec19223f95659d3fbe387591d240c44336c1
daria_smyr <dasha.smyr@gmail.com>
2022-12-03 14:54:48 +0300

    Small fixes

--- src/app.js
+++ src/app.js
@@ -1,33 +1,43 @@
 const config = require('./sample-config.json');
 const axios = require('axios');
+const chalk = require('chalk');
 
 function log(...msg) {
-    console.log(new Date().toISOString(), ...msg);
+    console.log(chalk.bgCyanBright(new Date().toISOString()), '|', chalk.magenta(...msg));
 }
 
 async function pingSite(url) {
+    // Download url and check status is 200
+    const result = await axios.get(url);
     try {
-        // Download url and check status is 200
-        const result = await axios.get(url);
-        return (result.status >= 200) && (result.status <= 299);
+    return (result.status >= 200) && (result.status <= 299);
     } catch (err) {
-        log('err', err.message);
-        return false;
+    return false;
     }
 }
 
 function main() {
     console.log('Starting...');
     const sites = config.sites;
-    log('sites', sites);
+    log('sites', JSON.stringify(sites, null, 2));
     for (const site of sites) {
         log(`Start pinging ${site.url} with interval ${site.intervalMs}`);
         setInterval(async function () {
             const pingTimeStart = new Date().getTime();
-            const pingResult = await pingSite(site.url);
-            const pingTimeEnd = new Date().getTime();
-            const pingTime = pingTimeEnd - pingTimeStart;
-            log(`Ping ${site.url} result: ${pingResult} in ${pingTime} ms`);
+            let pingResult = undefined;
+            let pingTime = undefined;
+            let pingTimeEnd = undefined;
+            try {
+                pingResult = await pingSite(site.url);
+                pingTimeEnd = new Date().getTime();
+                pingTime = pingTimeEnd - pingTimeStart;
+                log(`Ping ${site.url} result: ${pingResult} in ${pingTime} ms`);
+            } catch (err) {
+                pingResult = false;
+                pingTimeEnd = new Date().getTime();
+                pingTime = pingTimeEnd - pingTimeStart;
+                log(`Ping ${site.url} result: ${pingResult} in ${pingTime} ms. Error: ${err.message}`);
+            }
         }, site.intervalMs);
     }
     log('Monitoring sites...');
