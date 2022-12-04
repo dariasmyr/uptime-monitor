a0c9bd8e13467f63e7890f56cb07573907e51dff
daria_smyr <dasha.smyr@gmail.com>
2022-12-04 15:41:18 +0300

    Deleted config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-const config = require('./config-example.js');
-// Example: override private key of config-example.js
-config.privateKey = '0x123213243243';
-// Export results from config-example.js
-module.exports = config;
