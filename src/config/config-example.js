2531dfeee9c66612b22df8d80d48f3ac4e56e940
daria_smyr <dasha.smyr@gmail.com>
2022-12-05 00:30:19 +0300

    Small improvement in config-example.js

--- src/config/config-example.js
+++ src/config/config-example.js
@@ -1,23 +1,15 @@
 const config = {
-    apiKey: "000000:0000000",
-    chatId: "000",
+    apiKey: "0000000000:00000000000000000000000000000000000",
+    chatId: "0000000",
     sites: [
         {
-            url: "https://gyde.one/",
+            url: "https://site.com",
             intervalMs: 6000
         },
         {
-            url: "http://stage.gyde.one/",
-            intervalMs: 6100
-        },
-        {
-            url: "https://identix.space/",
-            intervalMs: 6200
-        },
-        {
-            url: "https://www.imdb.com/title/tt1160419/",
-            intervalMs: 6300
-        },
+            url: "http://site2.com",
+            intervalMs: 3000
+        }
     ]
 }
 
