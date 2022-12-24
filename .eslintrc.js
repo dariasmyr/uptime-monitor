eabfc0d156ebc69a789645eb8a297706faa90bec
daria_smyr <dasha.smyr@gmail.com>
2022-12-24 14:16:06 +0300

    Added sonar lib and updated axios

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
         'commonjs': true,
         'es2021': true
     },
-    'extends': 'eslint:recommended',
+    'root': true,
+    'extends': ['eslint:recommended', ],
     'overrides': [
     ],
     'parserOptions': {
