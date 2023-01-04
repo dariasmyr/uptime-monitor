0cceec2615bdcefa002e89cbc3833d9d71126a42
daria_smyr <dasha.smyr@gmail.com>
2023-01-04 12:28:48 +0300

    Fixed ssl.certificate.check.service.js

--- src/ssl-checker/ssl.certificate.check.service.js
+++ src/ssl-checker/ssl.certificate.check.service.js
@@ -5,36 +5,50 @@ class SslCertificateCheckService {
   constructor() {
     this.logger = new LoggerService('SslCertificateService');
   }
-
-  async getSiteCertificate(host) {
-    const socket = tls.connect({
-      host: host,
+  async getCertInfo(_host, _timeout) {
+    const options = {
+      host: _host,
       port: 443,
-      servername: host
-    }, () => {
-      const peerCertificate = socket.getPeerCertificate();
-      console.log(peerCertificate);
-      const validTo = peerCertificate.valid_to;
-      const validToMs = Date.parse(validTo);
-      const nowMs = Date.now();
-      const daysLeft = Math.round((validToMs - nowMs) / (1000 * 60 * 60 * 24));
-      console.log(`DAYS LEFT: ${daysLeft}`);
-      if (daysLeft <= 0) {
-        this.logger.error(`SSL certificate for ${host} is expired. Valid to: ${validTo} days.`);
-        return -1;
-        // close socket
-      } else {
-        this.logger.debug(`SSL certificate for ${host} is valid. Valid to: ${validTo} days. Days left: ${daysLeft}.`);
-        return daysLeft;
-      }
+      servername: _host
+    };
+    const timeout = _timeout;
+    return new Promise((resolve, reject) => {
+      const result = {};
+      const socket = tls.connect(options);
+      socket.setTimeout(timeout);
+      socket.once('secureConnect', () => {
+        const peerCertificate = socket.getPeerCertificate();
+        result.validFrom = peerCertificate.valid_from;
+        result.validTo = peerCertificate.valid_to;
+        socket.destroy();
+      });
+      socket.once('close', () => {
+        resolve(result);
+      });
+      socket.once('error', (error) => {
+        reject(error);
+      });
+      socket.once('timeout', () => {
+        reject(new Error(`Timeout after ${timeout} ms for ${options}`));
+      });
     });
-    socket.on('error', error => {
-      console.log(`Error: ${ error.message}`);
-    });
-    socket.on('close', () => {});
+  }
+
+  // calculate the remaining days of this certificate
+  async getRemainingDays(_host, _timeout) {
+    const certInfo = await this.getCertInfo(_host, _timeout);
+    const certValidTo = new Date(certInfo.validTo).valueOf();
+    const now = Date.now();
+    if (certValidTo < now) {
+      this.logger.error('Certificate is expired');
+      return -1;
+    } else {
+      const remainingDays = Math.floor((certValidTo - now) / 1000 / 60 / 60 / 24);
+      this.logger.debug(`Certificate is valid for ${remainingDays} days`);
+      return remainingDays;
+    }
   }
 }
-
 
 module.exports = {
   SslCertificateCheckService
