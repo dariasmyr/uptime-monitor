f46c18983654febff6a11a3963aa5c1b1944d19c
daria_smyr <dasha.smyr@gmail.com>
2023-01-04 12:29:09 +0300

    Fixed test for ssl.certificate.check.service.js

--- src/ssl-checker/ssl.certificate.check.service.test.js
+++ src/ssl-checker/ssl.certificate.check.service.test.js
@@ -4,8 +4,17 @@ const {SslCertificateCheckService} = require('./ssl.certificate.check.service.js
 describe('Check SSL certificate service', () => {
   const sslCertificateCheckService = new SslCertificateCheckService();
 
-  test('should show SSL certificate', async () => {
-    const results = await sslCertificateCheckService.getSiteCertificate('medium.com');
-    expect(results).toBeGreaterThan(0);
+  test('should show SSL certificate info', async () => {
+    const certInfo = await sslCertificateCheckService.getCertInfo('gyde.one', 1000);
+    const expected = {
+      validFrom: 'Dec 25 12:47:27 2022 GMT',
+      validTo: 'Mar 25 12:47:26 2023 GMT'
+    };
+    expect(certInfo).toMatchObject(expected);
+  });
+
+  test('should show SSL certificate days left', async () => {
+    const remainingDays = await sslCertificateCheckService.getRemainingDays('gyde.one', 1000);
+    expect(remainingDays).toBeGreaterThan(0);
   });
 });
