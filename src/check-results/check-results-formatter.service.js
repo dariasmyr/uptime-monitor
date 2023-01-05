d7cce0e319380b93ddbb2015604b4c6120c6b238
daria_smyr <dasha.smyr@gmail.com>
2023-01-05 14:54:59 +0300

    Small fix

--- src/check-results/check-results-formatter.service.js
+++ src/check-results/check-results-formatter.service.js
@@ -1,47 +1,6 @@
 const CheckResultsFormatterService = {
   // eslint-disable-next-line sonarjs/cognitive-complexity,complexity
   formatResults(httpResults, pingResults, sslResults) {
-    /*
- Examples:
- HTTP results:
-        {
-  "http://stage.gyde.one/": {
-    "isAlive": false,
-    "message": "getaddrinfo ENOTFOUND stage.gyde.one"
-  },
-  "https://gyde.one/": {
-    "isAlive": true,
-    "message": "HTTP status 200"
-  },
-  "https://www.imdb.com/title/tt0137523/": {
-    "isAlive": false,
-    "message": "Request failed with status code 403"
-  }
-}
-
-        Ping results:
-        {
-  "stage.gyde.one": {
-    "isAlive": false,
-    "time": -1
-  },
-  "gyde.one": {
-    "isAlive": true,
-    "time": 83
-  },
-  "www.imdb.com": {
-    "isAlive": true,
-    "time": 53
-  }
-}
-
-        SSL results:
-        {
-  "stage.gyde.one": "Certificate for host stage.gyde.one is expired or doesn't exist. Error: ENOTFOUND",
-  "gyde.one": "Host gyde.one is active. Certificate will expire in 594625748112 days.",
-  "www.imdb.com": "Host www.imdb.com is active. Certificate will expire in 2143358967139 days."
-}
-         */
 
     const results = new Map();
 
