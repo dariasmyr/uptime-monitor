c10838ca7b39eacab4aa4dcb1973dd17381c9f37
daria_smyr <dasha.smyr@gmail.com>
2023-01-27 16:35:58 +0300

    Added prisma

---
+++ prisma/migrations/20230127133309_uptime_2_0/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Post";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Uptime" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "host" TEXT NOT NULL,
+    "healthCheckIsAlive" TEXT NOT NULL,
+    "healthCheckResponseBody" TEXT NOT NULL,
+    "httpCheckIsAlive" TEXT NOT NULL,
+    "httpCheckMessage" TEXT NOT NULL,
+    "pingCheckIsAlive" TEXT NOT NULL,
+    "pingCheckTimeMs" INTEGER NOT NULL,
+    "sslCheckIsAlive" TEXT NOT NULL,
+    "sslCheckDaysLeft" INTEGER NOT NULL
+);
