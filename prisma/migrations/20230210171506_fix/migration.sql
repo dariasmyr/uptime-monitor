5fe92f7222cf075df07f5e51b540a9e23d4b2011
daria_smyr <dasha.smyr@gmail.com>
2023-02-10 21:28:11 +0300

    Added migrations

---
+++ prisma/migrations/20230210171506_fix/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `checkResolutions` on the `Uptime` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Uptime" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "host" TEXT NOT NULL,
+    "healthCheckIsAlive" TEXT NOT NULL,
+    "healthCheckResponseBody" TEXT NOT NULL,
+    "httpCheckIsAlive" TEXT NOT NULL,
+    "httpCheckStatusCode" INTEGER,
+    "pingCheckIsAlive" TEXT NOT NULL,
+    "pingCheckTimeMs" INTEGER NOT NULL,
+    "sslCheckIsAlive" TEXT NOT NULL,
+    "sslCheckDaysLeft" INTEGER NOT NULL,
+    "checkResolution" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_Uptime" ("createdAt", "healthCheckIsAlive", "healthCheckResponseBody", "host", "httpCheckIsAlive", "httpCheckStatusCode", "id", "pingCheckIsAlive", "pingCheckTimeMs", "sslCheckDaysLeft", "sslCheckIsAlive") SELECT "createdAt", "healthCheckIsAlive", "healthCheckResponseBody", "host", "httpCheckIsAlive", "httpCheckStatusCode", "id", "pingCheckIsAlive", "pingCheckTimeMs", "sslCheckDaysLeft", "sslCheckIsAlive" FROM "Uptime";
+DROP TABLE "Uptime";
+ALTER TABLE "new_Uptime" RENAME TO "Uptime";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
